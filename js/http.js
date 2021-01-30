1650b081ff2fc2ee8266131356433839b6a58533
hujiafu <hjf123456>
2021-01-30 16:40:19 +0800

    修改首页样式

--- js/http.js
+++ js/http.js
@@ -1,5 +1,5 @@
 //测试的接口地址
-var hisName = "出入登记系统"
+var hisName = "城口县"
 
 var host="http://gbdatatest.mx5918.com/index";
 var baseUrl="http://gbdatatest.mx5918.com/"
@@ -15,10 +15,11 @@ var in_submit_fun="/index/entry";//进入登记
 var out_submit_fun="/index/out";//出去登记
 var file_fun="/index/upload";//上传图片
 var sacan_file_fun="/index/card";//识别身份证
+var business_community_fun="/business/community";//社区
+var GetBanners="/index/getBanner";//获取banner
 var shop_index_fun="/shop/index";//商铺登记
 var business_index_fun="/business/index";//企业登记
 var index_license_fun="/index/license";//识别营业执照
-var business_community_fun="/business/community";//社区
 var index_business_search_fun="/business/search";//查询企业或者商铺信息
 var index_business_edit_fun="/business/edit";//企业编辑
 var index_shop_edit_fun="/shop/edit";//商铺编辑
@@ -26,7 +27,6 @@ var index_Land_Contract_fun="/Land_Contract";//添加承包合同
 var Land_Contract_search_fun="/Land_Contract/search";//搜索合同
 var Land_Contract_edit_fun="/Land_Contract/edit";//编辑合同
 var Land_Contract_editSave_fun="/Land_Contract/editSave";//保存编辑合同
-var GetBanners="/index/getBanner";//banner
 var NewContract="/contract/addContract";//新建合同
 var SearchContract="/contract/searchContract";//搜索合同
 var GetContractInfo="/contract/getContractInfo";//合同详情
